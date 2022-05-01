789b362d17643f1696bd9063769737f09e43e85d
unknown <marie.parizeau@gmail.com>
2022-05-01 19:37:50 -0400

    Added specific switch to scripts for Stream Deck

---
+++ SwitchTo/SwitchToDesktop.ps1
@@ -0,0 +1,32 @@
+﻿if ((Get-InstalledModule `
+    -Name "AudioDeviceCmdlets" `
+    -ErrorAction SilentlyContinue) -eq $null) {
+    Install-Module -Name AudioDeviceCmdlets 
+}
+
+function Get-Device-Id {
+    param (
+        [ValidateNotNullOrEmpty()]
+        [string]$Type,
+        [ValidateNotNullOrEmpty()]
+        [string]$Name
+    )
+
+    return Get-AudioDevice -list |
+        where Type -eq $Type |
+        Where-Object Name -eq $Name | 
+        ForEach-Object Index
+
+}
+
+# Playback Device
+$Desktop =  Get-Device-Id 'Playback' 'Headphones (HyperX Cloud Flight S Game)'
+if($Desktop){
+	{set-audiodevice -index $Desktop} 
+}
+
+#Recording Device
+$Desktop = Get-Device-Id 'Recording' 'Microphone (HyperX QuadCast S)'
+if($Desktop){
+	{set-audiodevice -index $Desktop} 
+}
---
+++ SwitchTo/SwitchToVR.ps1
@@ -0,0 +1,31 @@
+﻿if ((Get-InstalledModule `
+    -Name "AudioDeviceCmdlets" `
+    -ErrorAction SilentlyContinue) -eq $null) {
+    Install-Module -Name AudioDeviceCmdlets 
+}
+
+function Get-Device-Id {
+    param (
+        [ValidateNotNullOrEmpty()]
+        [string]$Type,
+        [ValidateNotNullOrEmpty()]
+        [string]$Name
+    )
+
+    return Get-AudioDevice -list |
+        where Type -eq $Type |
+        Where-Object Name -eq $Name | 
+        ForEach-Object Index
+}
+
+# Playback Device
+$Vr = Get-Device-Id 'Playback' 'Index HMD (NVIDIA High Definition Audio)'
+if($Vr){
+	set-audiodevice -index $Vr
+}
+
+#Recording Device
+$Vr = Get-Device-Id 'Recording' 'Digital Audio Interface (Valve VR Radio & HMD Mic)'
+if($Vr){
+	set-audiodevice -index $Vr
+}
